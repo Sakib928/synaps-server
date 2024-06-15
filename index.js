d2cab51365fa67de4b51ace7c35cb31168e9b33f
Sakib928 <sakibgscian@gmail.com>
2024-06-15 22:27:17 +0600

    added user role request api

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1towayy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,7 +32,7 @@ async function run() {
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
+        const userCollection = client.db('Synaps').collection('users');
 
         // jwt related apis
         app.post('/jwt', async (req, res) => {
@@ -41,6 +43,54 @@ async function run() {
             })
             res.send({ token });
         })
+
+        //custom middlewares
+        const verifyToken = async (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' });
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ status: 'forbidden access' })
+            }
+            next();
+        }
+
+
+        // user related apis
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const userExists = await userCollection.findOne(query);
+            if (userExists) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        // find user role
+        app.get('/role', async (req, res) => {
+            const email = req.query.email;
+            console.log('to find the role:', email)
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            res.send(user?.role);
+        })
+
 
     } finally {
         // Ensures that the client will close when you finish/error
